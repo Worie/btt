d33becc04c78e8a70bd5f6afb5c2247420c1f69c
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-07-23 11:16:17 +0200

    fix typings issues

--- btt/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for btt
-// Definitions by: wojciech.polowniak@gmail.com https://mozillians.org/pl/u/wopolow/
-
-export * from '../src/index';
-import '../definitions/global';
-import '../src/types'; 
