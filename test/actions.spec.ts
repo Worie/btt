f2fd38da688fc0b6248753ff2b29e2ae282b60ac
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-09-01 16:31:07 +0200

    remove any casting

--- test/actions.spec.ts
+++ test/actions.spec.ts
@@ -9,11 +9,11 @@ describe('Trigger shortcut action', () => {
   });
 
   // @TODO: fix typings
-  const action = (btt.triggerShortcut as any)('cmd+space');
+  const action = btt.triggerShortcut('cmd+space');
 
   // bieda testy
   it('should match url', () => {
-    expect((btt.triggerShortcut as any)('cmd+space').url).toEqual('http://127.0.0.1:8000/trigger_action/?json=%7B%22BTTPredefinedActionType%22%3A-1%2C%22BTTEnabled2%22%3A1%2C%22BTTEnabled%22%3A1%2C%22BTTShortcutToSend%22%3A%2255%2C49%22%7D');
+    expect(btt.triggerShortcut('cmd+space').url).toEqual('http://127.0.0.1:8000/trigger_action/?json=%7B%22BTTPredefinedActionType%22%3A-1%2C%22BTTEnabled2%22%3A1%2C%22BTTEnabled%22%3A1%2C%22BTTShortcutToSend%22%3A%2255%2C49%22%7D');
   });
 
 });
